2596f4840e27cb2a7ea313da79a57e10f3964211
Marshalldt1 <98995036+Marshalldt1@users.noreply.github.com>
2022-04-13 15:04:46 -0300

    commit init
    primeiros passos

---
+++ script.js
@@ -0,0 +1,15 @@
+function voltar() {
+   document.getElementById("changerimg").src="image/shipp.jpg";
+}
+function sasuke() {
+   document.getElementById('changerimg').src="image/sasuke.jpeg";
+}
+function sakura() {changerimg
+   document.getElementById('changerimg').src="image/sakura.jpeg";
+}
+function minato() {
+   document.getElementById('changerimg').src="image/minato.jpg";
+}
+function naruto() {
+   document.getElementById('changerimg').src="image/naruto.jpg";
+}
